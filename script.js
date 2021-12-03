7ddba1701e1652383d26275f9dc252dc349004f4
Prisberg <fille.risberg@live.se>
2021-12-03 02:43:33 +0100

    bunchOfStuff

---
+++ script.js
@@ -0,0 +1,5 @@
+const track = document.querySelector('.section-container')
+
+/* when i click up, move to previous section */
+/* when i click dowm, move to next section */
+/* when i click the nav-links, move to selected section */
